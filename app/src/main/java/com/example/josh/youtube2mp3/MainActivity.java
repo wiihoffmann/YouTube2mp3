f1e5e3bb7c68bf889ad53775e904d5d79bdc01fd
Josh <wiihoffmann@yahoo.ca>
2017-12-15 23:30:27 -0700

    download button has a method run on click

--- app/src/main/java/com/example/josh/youtube2mp3/MainActivity.java
+++ app/src/main/java/com/example/josh/youtube2mp3/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.josh.youtube2mp3;
 
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
+        import android.util.Log;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+    }
+
+    public void download(View v){
+        EditText text = findViewById(R.id.URLBox);
+        String str = text.getText().toString();
+        Log.d("#######################", str);
 
     }
 
